f1545e276c00a7f92952857bf9c22a717406067b
pushkar <pushkarkolhe@gmail.com>
2014-08-01 19:41:41 -0700

    changed database settings

--- peer/settings.py
+++ peer/settings.py
@@ -79,6 +79,9 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
