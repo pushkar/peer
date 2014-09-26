a6a9fa659d65e21f985649a7e9691ff8b4a15860
pushkar <pushkarkolhe@gmail.com>
2014-09-26 16:17:32 -0400

    New static file settings

--- peer/settings.py
+++ peer/settings.py
@@ -108,7 +108,7 @@ if os.getcwd() == "/app":
 # Static asset configuration
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
