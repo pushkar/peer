b33c105989101b0c5b107fb7e70101fd24a0d659
pushkar <pushkarkolhe@gmail.com>
2014-11-23 13:46:40 -0500

    Bug fixed sl/urls.py

--- sl/urls.py
+++ sl/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, url
 
-from sl import views, views_admin
+from sl import views
 
 urlpatterns = patterns('',
     url(r'^login$', views.login, name='login'),
@@ -12,10 +12,6 @@ urlpatterns = patterns('',
 
     url(r'^exam/tf$', views.exam_tf, name='exam_tf'),
     url(r'^exam/mc$', views.exam_mc, name='exam_mc'),
-    url(r'^grade/(?P<log_type>\w+)/(?P<log_id>\d+)/(?P<score>\d+.\d+)$', views.grade, name='grade'),
-    url(r'^grade/auto$', views.grade_auto, name='grade_auto'),
-    url(r'^grade/(?P<log_type>\w+)/(?P<log_id>\d+)$', views.grade_log, name='grade_log'),
-    url(r'^grade/student/(?P<u_id>\w+)$', views.grade_student, name='grade_student'),
     url(r'^db/populate$', views.db_populate, name='db_populate'),
     url(r'^db/show$', views.db_show, name='db_show'),
     url(r'^$', views.index, name='index'),
