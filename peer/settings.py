f536d4971b16e641736599c0cff1768141ea8652
pushkar <pushkarkolhe@gmail.com>
2015-02-14 11:48:40 -0500

    Adds Google Analytics

--- peer/settings.py
+++ peer/settings.py
@@ -31,7 +31,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_mathjax',
-    'ganalytics',
     'student',
     'recommender',
     'assignment',
