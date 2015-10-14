67d8a206a9cf9f75df86a4be718c5ba541f128dc
Pushkar Kolhe <pushkarkolhe@gmail.com>
2015-10-14 19:07:56 -0400

    Creates exams and saves them. Doesn't check when the student tries the exam again.

--- exam/urls.py
+++ exam/urls.py
@@ -4,5 +4,8 @@ from exam import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^save$', views.save_exam, name='save_exam'),
+    url(r'^(?P<exam_name>\w+)$', views.get_or_create_exam, name='get_or_create_exam'),
+    url(r'^(?P<exam_name>\w+)/save$', views.save_exam, name='save_exam'),
+    url(r'^(?P<exam_name>\w+)/submit$', views.submit_exam, name='submit_exam'),
+
 )
--- exam/views.py
+++ exam/views.py
@@ -3,23 +3,45 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.contrib import messages
 from django import template
-from student.models import *
-
 from django_ajax.decorators import ajax
 
+from student.models import *
 from exam_info import *
 import json
 
 def index(request):
     s = Student.objects.get(username=request.session['user'])
-    e = exam_info('midterm')
-    tempexam = tempexam_info(s, e.get_exam())
-    return render(request, 'exam_create.html', {
+    exams = Exam.objects.all()
+    return render(request, 'exam_index.html', {
             'student': s,
-            'exam': e.get_exam(),
-            'tempexam': tempexam.create_exam(),
+            'exams': exams,
         })
 
-def save_exam(request):
-    print request.POST.lists()
+def get_or_create_exam(request, exam_name):
+    s = Student.objects.get(username=request.session['user'])
+    e = exam_info(exam_name)
+    tempexam = tempexam_info(s, e.get_exam())
+    return render(request, 'exam_get_or_create.html', {
+            'student': s,
+            'exam': e.get_exam(),
+            'tempexam': tempexam.get_exam(),
+        })
+
+def save_exam(request, exam_name):
+    s = Student.objects.get(username=request.session['user'])
+    e = exam_info(exam_name)
+    tempexam = tempexam_info(s, e.get_exam())
+    if request.POST:
+        data = dict(request.POST.iterlists())
+        tempexam.save_exam(data)
+        messages.success(request, "Your exam was saved successfully.")
+    return HttpResponseRedirect(reverse('exam:get_or_create_exam', args=[exam_name]))
+
+def submit_exam(request, exam_name):
+    s = Student.objects.get(username=request.session['user'])
+    e = exam_info(exam_name)
+    tempexam = tempexam_info(s, e.get_exam())
+    if tempexam.submit_exam():
+        messages.success(request, "Your exam was submitted successfully.")
+        tempexam.delete_exam()
     return HttpResponseRedirect(reverse('exam:index'))
