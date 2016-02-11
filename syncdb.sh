8d6232f65d4a40bc21aa4c0dae12d1349bd35919
Pushkar Kolhe <pushkarkolhe@gmail.com>
2016-02-11 00:09:50 -0500

    Changed syncdb script

--- syncdb.sh
+++ syncdb.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-heroku autobus capture --app rldm DATABASE_URL
-rm latest.dump
-curl -o latest.dump `heroku pg:backups public-url --app rldm`
+#heroku autobus capture --app rldm DATABASE_URL
+#rm latest.dump
+#curl -o latest.dump `heroku pg:backups public-url --app rldm`
 pg_restore --verbose --clean --no-acl --no-owner -h localhost -U pushkar -d rlspring2016 latest.dump
