fa10998f7119e8d09f3c623d2fe185d67f9e6131
Pushkar Kolhe <pushkar.query@gmail.com>
2017-06-14 18:38:46 -0400

    Adds script to update IOSolution through API (#9)
    * Adds favicons
    
    * Corrects the get_hw script
    
    * Can update IOSolutions now
    
    * Set debug to False

---
+++ scripts/README.md
@@ -0,0 +1,14 @@
+scripts
+------
+
+* add_roster.py
+
+Adds roster to the peer website. The first row corresponds to the column title for each entry (username, usertype, email, email_tsq, gtid, lastname, firstname)
+
+* get_hw.py
+
+Get student's homeworks. If there are 10 students, who answer 5 problems.. then there are 50 iosolutions. Configure the homework short_name in the script.
+
+* set_hw_to_gradebook.py
+
+Put homework scores into gradebook/t-sq assignment. Download the scores csv from the peer website and then use the script. Configure column names in the script.
--- scripts/get_hw.py
+++ scripts/get_hw.py
@@ -2,37 +2,51 @@ import sys
 import json
 import requests
 
-assignment_short_name = 'td'
+assignment_short_name = 'td' # get from peer website
 url_base = 'https://rldm.herokuapp.com/api/'
-data_login = {}
-data_login['username'] = ''
-data_login['gtid'] = ''
+data_login = {
+    'username': '',
+    'gtid': ''
+}
 
 url_login = url_base + 'login'
 r = requests.post(url_login, data=data_login, headers=dict(Referer=url_login))
 
 data = json.loads(r.text)
+key = data['key']
 print("Login: %s %s" % (r.status_code, r.text))
-print("Key is %s" % data['key'])
+print("Key is %s" % key)
 
 url_get_hw = url_base + 'codework/assignment/' + assignment_short_name
 r = requests.get(url_get_hw, params=data)
 print(r.url)
-response = json.loads(r.text)
-if 'error' in response:
-    print("Error: %s" % response['error'])
+print("You can call this url from a browser for the next 5 mins")
+responses = json.loads(r.text)
+if 'error' in responses:
+    print("Error: %s" % responses['error'])
+    print("Run again.")
     sys.exit(1)
 
-score = {}
-for hw_model in response:
-    hw = hw_model['fields']
+for response in responses:
+    iosolution_id = response['id']
+    iosolution_info = response['solution']
+    iopair_info = response['pair']
+    student_info = response['student']
 
-    student_id = hw['student']
-    student_score = hw['score']
-    if student_id not in score:
-        score[student_id] = 0
+# Update IOSolutions
+for response in responses:
+    iosolution_id = response['id']
+    url_update_sol = url_base + 'codework/solution/%s' % iosolution_id
+    data = {
+        'key': key,
+        'score': 10,
+        'submission': 'this is their new submission',
+        'comments': 'these are some new comments',
+    }
+    r = requests.post(url_update_sol, data=data, headers=dict(Referer=url_login))
+    r = json.loads(r.text)
+    if 'error' in r:
+        print("Error: %s" % responses['error'])
+        sys.exit(1)
+    print(r['message'])
 
-    score[student_id] += student_score
-
-for k, v in score.items():
-    print(k, v)
