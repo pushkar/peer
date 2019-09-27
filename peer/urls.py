5caf80829ac7df8cf3e9940f1a0395713a54e97f
Pushkar Kolhe <pushkar@moquality.com>
2019-09-26 22:15:19 -0400

    Adds checking for the sarsa homework (#25)

--- peer/urls.py
+++ peer/urls.py
@@ -6,20 +6,11 @@ import django.contrib.staticfiles.views as static_views
 admin.autodiscover()
 
 urlpatterns = [
-    # url(r'^student/', 'student', name='student'),
-
-    # url(r'^student/', include('student.urls', namespace="student")),
-    # url(r'^assignment/', include('assignment.urls', namespace="assignment")),
-
-
-
     path('', views.index, name='index'),
     path('student/', include('student.urls')),
     path('assignment/', include('assignment.urls')),
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
     path('static/<slug:path>', static_views.serve),
-
     # path('exam/', include('exam.urls')),
-
 ]
